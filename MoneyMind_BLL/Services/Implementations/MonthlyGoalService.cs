1f7f6ce4debb408cc82f17aa462c05635726ca0c
Dwuangne <tquang0809@gmail.com>
2025-03-05 08:39:04 +0700

    fix bug

--- MoneyMind_BLL/Services/Implementations/MonthlyGoalService.cs
+++ MoneyMind_BLL/Services/Implementations/MonthlyGoalService.cs
@@ -50,30 +50,19 @@ namespace MoneyMind_BLL.Services.Implementations
                                                .Item1
                                                .Select(item => item.Id);
 
-            //// Danh sách WalletTypeId mặc định
-            //var defaultWalletTypeIds = new List<Guid>
-            //{
-            //    Guid.Parse("B79D14DB-7A81-4046-B66E-1ACD761123BB"),
-            //    Guid.Parse("B203AE2F-3023-41C1-A25A-2B2EC238321D"),
-            //    Guid.Parse("6193FCB1-C8C4-44E9-ABDE-78CDB4258C4E"),
-            //    Guid.Parse("654A9673-4D23-44B1-9AF8-A9562341A60E"),
-            //    Guid.Parse("19EA7E67-8095-4A13-BBA4-BDA0A4A47A38"),
-            //    Guid.Parse("EBEBC667-520D-4EAC-88ED-EF9EB8E26AAB")
-            //};
-
             var goalItems = new List<GoalItem>();
 
             foreach (var walletTypeId in walletTypedIds)
             {
-                var usedAmount = await transactionRepository.GetSumAsync(
-                    t => t.UserId == userId
-                      && t.Wallet != null
-                      && t.Wallet.WalletCategory != null
-                      && t.Wallet.WalletCategory.WalletTypeId == walletTypeId
-                      && t.TransactionDate.Month == monthlyGoalDomain.Month
-                      && t.TransactionDate.Year == monthlyGoalDomain.Year,
-                    t => t.Amount
-                );
+                //var usedAmount = await transactionRepository.GetSumAsync(
+                //    t => t.UserId == userId
+                //      && t.Wallet != null
+                //      && t.Wallet.WalletCategory != null
+                //      && t.Wallet.WalletCategory.WalletTypeId == walletTypeId
+                //      && t.TransactionDate.Month == monthlyGoalDomain.Month
+                //      && t.TransactionDate.Year == monthlyGoalDomain.Year,
+                //    t => t.Amount
+                //);
 
                 var goalItem = new GoalItem
                 {
@@ -85,8 +74,10 @@ namespace MoneyMind_BLL.Services.Implementations
                     MaxTargetPercentage = goalDefaults.MaxTargetPercentage, // Mặc định có thể thay đổi
                     MinAmount = goalDefaults.MinAmount,  // Giá trị tối thiểu mặc định
                     MaxAmount = goalDefaults.MaxAmount, // Chia đều ngân sách
-                    UsedAmount = usedAmount,
-                    UsedPercentage = (monthlyGoalDomain.TotalAmount > 0) ? (usedAmount / monthlyGoalDomain.TotalAmount) * 100 : 0
+                    //UsedAmount = usedAmount,
+                    //UsedPercentage = (monthlyGoalDomain.TotalAmount > 0) ? (usedAmount / monthlyGoalDomain.TotalAmount) * 100 : 0
+                    UsedAmount = 0,
+                    UsedPercentage = 0,
                 };
 
                 goalItems.Add(goalItem);
